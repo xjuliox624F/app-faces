0caf4cf22f025881be7612e224a4acf6c0207b17
xjuliox624F <95454272+xjuliox624F@users.noreply.github.com>
2025-03-20 11:04:56 -0600

    Update faces-model.py

--- faces-model.py
+++ faces-model.py
@@ -68,7 +68,7 @@ model.add(Dense(len(listPersons), activation='softmax'))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(X_train.reshape(-1, 150, 150, 1), y_train, epochs=10, validation_data=(X_test.reshape(-1, 150, 150, 1), y_test))
+model.fit(X_train.reshape(-1, 150, 150, 1), y_train, epochs=50, validation_data=(X_test.reshape(-1, 150, 150, 1), y_test))
 
 export_path = 'face-model/1/'
 tf.saved_model.save(model, os.path.join('./',export_path))
