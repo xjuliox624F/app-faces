2e68e51face71be2f6a7ce9e6607e5872769142e
xjuliox624F <95454272+xjuliox624F@users.noreply.github.com>
2025-04-11 14:44:30 -0600

    Update faces-model.py

--- faces-model.py
+++ faces-model.py
@@ -22,7 +22,7 @@ data = pathlib.Path(directory)
 
 labels = []
 images = []
-listPersons = ['Chris Evans', 'Chris Hemsworth', 'Mark Ruffalo', 'Robert Downey Jr', 'Scarlett Johansson']
+listPersons = ['Angel', 'Arkel', 'gabriel', 'Guadalupe','julio' ]
 
 size = 64,64
 print('folders')
