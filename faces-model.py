3d0c2e15eceb58a4362c994e8a9cfa531f11e78a
xjuliox624F <95454272+xjuliox624F@users.noreply.github.com>
2025-04-11 14:41:12 -0600

    Update faces-model.py

--- faces-model.py
+++ faces-model.py
@@ -11,7 +11,7 @@ import pathlib
 
 # Set the path of the input folder
 
-dataset = "https://drive.google.com/uc?export=download&id=1IXx1TynW-WmUlNNbJEvnExXDrmDvQlT3"
+dataset = "https://drive.google.com/uc?export=download&id=1ThUklHdF9_LhujOFvg6-2kxqB79vnO92"
 directory = tf.keras.utils.get_file('caras', origin=dataset, untar=True)
 data = pathlib.Path(directory)
 
